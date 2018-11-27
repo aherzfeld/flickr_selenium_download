fc08fb1328f2da9e8505aedf08ebf9cc2349e5b5
aherzfeld <herzfeld.andrew@gmail.com>
2018-11-27 21:30:15 +1100

    modified firefox settings to avoid dl popup

--- flickr_dl.py
+++ flickr_dl.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.options import Options
 
 
 # TODO: get user arg strings for category and number to dl
@@ -17,20 +18,6 @@ search_term = sys.argv[1]
 num_to_dl = int(sys.argv[2])
 print('Searching "{}" on Flickr and downloading {} images.'.format(
       search_term, num_to_dl))
-
-# TODO: instantiate driver and get website + search
-driver = webdriver.Firefox()
-driver.get('https://www.flickr.com/search/?text=' + search_term)
-
-# TODO: find and click first photo element
-try:
-    photo_elem = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'a.overlay'))
-    )
-except TimeoutException:
-    photo_elem = driver.find_element_by_css_selector('a.overlay')
-    print('Timeout Exception was raised')
-photo_elem.click()
 
 # TODO: create folder in working directory
 n = 1
@@ -47,6 +34,29 @@ try:
 except FileExistsError as e:
     print(e)
 
+# TODO: instantiate driver and adjust firefox settings to avoid dl popup
+#driver = webdriver.Firefox()
+# TODO: ADD FIREFOX SETTINGS HERE!!!!!!!!!!!!!
+options = Options()
+options.set_preference("browser.download.folderList", 2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir", path)
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "image/jpeg")
+driver = webdriver.Firefox(options=options)
+
+# TODO: get website + search query
+driver.get('https://www.flickr.com/search/?text=' + search_term)
+
+# TODO: find and click first photo element
+try:
+    photo_elem = WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'a.overlay'))
+    )
+except TimeoutException:
+    photo_elem = driver.find_element_by_css_selector('a.overlay')
+    print('Timeout Exception was raised')
+photo_elem.click()
+
 # TODO download n photos
 # TODO: loop num_to_dl times
 print('Trying to download {} photos.'.format(num_to_dl))
@@ -59,9 +69,6 @@ dl_elem = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'Square'))
 )
 dl_elem.click()
-# TODO: change default firefox settings to avoid dl popup
-
-
 
 # TODO: close driver 
 driver.close()
